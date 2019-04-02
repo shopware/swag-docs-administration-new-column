47f8d6811967acda6047063006a2077ec9d5be55
Patrick Stahl <p.stahl@shopware.com>
2019-04-02 08:48:52 +0200

    Add plugin source files

---
+++ AdministrationNewColumn.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace AdministrationNewColumn;
+
+use Shopware\Core\Framework\Plugin;
+
+class AdministrationNewColumn extends Plugin
+{
+}
---
+++ Resources/views/administration/main.js
@@ -0,0 +1,20 @@
+import { Component } from 'src/core/shopware';
+
+Component.override('sw-product-list', {
+    computed: {
+        productColumns() {
+            let columns = this.getProductColumns();
+
+            columns.push({
+                property: 'manufacturer.id',
+                dataIndex: 'manufacturer.id',
+                label: 'Manufacturer ID',
+                inlineEdit: 'string',
+                allowResize: true,
+                align: 'left'
+            });
+
+            return columns;
+        },
+    },
+});
